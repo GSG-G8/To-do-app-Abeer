709bb19d2be2d5084343157be4c03dab22a22d5c
“Abeer <abeerkhalid571@gmail.com>
2020-02-07 21:08:10 +0200

    create function addTodo #9

--- test.js
+++ test.js
@@ -1,7 +1,26 @@
-var test = require('tape');
-var logic = require('./logic');
+// var test = require('tape');
+var todoFunctions = require("./js/logic");
 
-test('Example test', function(t) {
-  t.pass();
-  t.end();
+// test('Example test', function(t) {
+//   t.pass();
+//   t.end();
+// });
+describe("Testing add todo ", () => {
+  test("create todo", () => {
+    const actual = todoFunctions.addTodo([], "todo");
+    const expected = [{ id: 1, description: "todo", done: false }];
+    expect(actual).toEqual(expected);
+  });
+
+  test("create new todo", () => {
+    const actual = todoFunctions.addTodo(
+      [{ id: 1, description: "todo", done: false }],
+      "todo 1"
+    );
+    const expected = [
+      { id: 1, description: "todo", done: false },
+      { id: 2, description: "todo 1", done: false }
+    ];
+    expect(actual).toEqual(expected);
+  });
 });
